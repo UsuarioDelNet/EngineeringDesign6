5f050dd6ac5e42fb14efb4f8b864a6c16c7ec467
Daniel L <104541075+UsuarioDelNet@users.noreply.github.com>
2025-04-28 01:33:57 -0400

    Add files via upload

---
+++ Labs/Lab1/CodeLab1/tff.vhd
@@ -0,0 +1,25 @@
+Library IEEE;
+use ieee.std_logic_1164.all;
+
+entity TFF is
+	port
+	(
+		din: in std_logic;
+		clk: in std_logic;
+		rst: in std_logic;
+		dout: out std_logic
+	);
+end TFF;
+
+architecture behavioral of TFF is 
+begin
+	process(rst,clk,din)
+	begin
+		if (rst='1') then
+			dout<='0';
+		elsif(rising_edge(clk)) then
+			dout<=not din;
+		end if;
+	end process;
+
+end behavioral;
